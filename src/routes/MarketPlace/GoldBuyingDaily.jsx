1f995af87ea6ec77e45bf6e7a79e7fccc6299d24
akhilthej <67720804+akhilthej@users.noreply.github.com>
2024-06-24 22:16:21 +0530

    changed gold

--- src/routes/MarketPlace/GoldBuyingDaily.jsx
+++ src/routes/MarketPlace/GoldBuyingDaily.jsx
@@ -1,27 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuth } from '../../hooks/GlobalProvider';
+import { GOLD_LIVE_PRICE } from '../../hooks/APIHooks';
 
-const PaymentForm = () => {
+
+import {GoldCoin} from '../../assets/data/Imagedata';
+
+const GoldBuying = () => {
+  const { user } = useAuth();
+  const [amount, setAmount] = useState(0);
+  const [gold, setGold] = useState(0);
+  const [referralCode, setReferralCode] = useState('');
+  const [goldPricePerGram, setGoldPricePerGram] = useState(0);
+  const [formattedGold, setFormattedGold] = useState('');
+
+  const amountValues = [0, 10, 20, 50, 100, 200, 500, 7430];
+
   const [formData, setFormData] = useState({
     merchantTransactionId: '',
     merchantUserId: 'MUID' + Date.now(),
     amount: '',
     merchantOrderId: '',
-    mobileNumber: '',
-    message: '',
-    email: '',
-    shortName: ''
+    mobileNumber: user.phonenumber,
+    message: 'Order For Digital Gold - Regular',
+    email: user.emailaddress,
+    shortName: 'BAT_DigitalGold_Regular',
+    orderlist: '', // Initialize as empty string
+    referralCode: referralCode,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  useEffect(() => {
+    const fetchGoldPrice = async () => {
+      try {
+        const response = await fetch(GOLD_LIVE_PRICE);
+        const data = await response.json();
+        const goldPrice = data.find(item => item.product_name === 'Gold');
+        if (goldPrice) {
+          setGoldPricePerGram(goldPrice.price);
+        } else {
+          console.error('Gold price not found in API response');
+        }
+      } catch (error) {
+        console.error('Error fetching gold price:', error);
+      }
+    };
+
+    fetchGoldPrice();
+  }, []);
+
+  useEffect(() => {
+    const calculateGoldAfterGST = () => {
+      const goldAfterGST = gold * (1 - 0.03); // Assuming 3% GST
+      const formattedGold = goldAfterGST.toFixed(8);
+      setFormattedGold(formattedGold);
+    };
+
+    calculateGoldAfterGST();
+  }, [gold]);
+
+  const handleSliderChange = (e) => {
+    const value = Number(e.target.value);
+    const selectedAmount = amountValues[value];
+    const calculatedGold = selectedAmount / goldPricePerGram;
+    setAmount(selectedAmount);
+    setGold(calculatedGold);
+
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      amount: selectedAmount,
+      merchantTransactionId: 'MTID' + Date.now(),
+      merchantOrderId: 'MOID' + Date.now(),
+      orderlist: `${calculatedGold.toFixed(8)} grams`, // Update orderlist with calculated gold grams
+    }));
   };
 
-  const handleSubmit = async (e) => {
+  const handleReferralCodeChange = (e) => {
+    const code = e.target.value;
+    setReferralCode(code);
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      referralCode: code,
+      orderlist: `${formattedGold} grams`, // Update orderlist with referral code
+    }));
+  };
+
+  const handlePayment = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://batchugold.com/apis/Store/PhonePe/PhonePe.php', {
+      const response = await fetch('https://batchugold.com/apis/PhonePe/DigitalGold/PhonePe.php', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -48,37 +114,65 @@ const PaymentForm = () => {
   };
 
   return (
-    <div className='mt-20'>
-      <h2>Payment Form</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Merchant Transaction ID:
-          <input type="text" name="merchantTransactionId" value={formData.merchantTransactionId} onChange={handleChange} required />
-        </label><br />
-        <label>
-          Amount:
-          <input type="number" name="amount" value={formData.amount} onChange={handleChange} required />
-        </label><br />
-        <label>
-          Mobile Number:
-          <input type="text" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} />
-        </label><br />
-        <label>
-          Email:
-          <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        </label><br />
-        <label>
-          Message:
-          <textarea name="message" value={formData.message} onChange={handleChange}></textarea>
-        </label><br />
-        <label>
-          Short Name:
-          <input type="text" name="shortName" value={formData.shortName} onChange={handleChange} />
-        </label><br />
-        <button type="submit">Pay</button>
-      </form>
+    <div className="p-5 flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+      
+      <div className="p-8 w-full max-w-md">
+
+
+      <div className="bg-gradient-to-r from-[#facc15] to-[#ca8a04] p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-4 text-center text-white">Buy Gold Daily</h1>
+        <label className="block mb-2 text-white font-bold ">Select Amount (INR):</label>
+
+        <div className="relative w-full bg-yellow-700 max-w-lg h-10 flex items-center rounded-lg">
+          <input
+            type="range"
+            min="0"
+            max={amountValues.length - 1}
+            step="1"
+            value={amountValues.indexOf(amount)}
+            onChange={handleSliderChange}
+            className="w-full appearance-none bg-transparent cursor-pointer "
+            style={{ zIndex: 10, position: 'relative' }}
+          />
+          <div
+            className="absolute top-1/2 transform -translate-y-1/2 h-8 w-8 flex items-center justify-center pointer-events-none"
+            style={{
+              left: `calc(${(amountValues.indexOf(amount) / (amountValues.length - 1)) * 100}% - 16px)`,
+              zIndex: 20
+            }}
+          >
+            <img src={GoldCoin} alt="indicator" className="h-full w-full" />
+          </div>
+        </div>
+
+        <div className="text-center mt-1">
+          Rs. {amount}
+        </div>
+       
+        <p className="mb-4 text-[12px]"> 
+          Receive: {formattedGold} grams
+        </p>
+        <button
+          onClick={handlePayment}
+          className="w-full bg-yellow-900 text-white py-2 rounded hover:bg-yellow-700"
+        >
+          Buy Now
+        </button>
+      </div>
+
+     <div className='px-5'>
+     <div className='p-5 bg-yellow-400 rounded-b-2xl'>
+      <label className="block mb-2 text-yellow-900 font-bold text-sm ">Referral Code:</label>
+        <input
+          type="text"
+          value={referralCode}
+          onChange={handleReferralCodeChange}
+          className="w-full px-2 mb-2 border rounded "
+        /></div></div>
+
+    </div>
     </div>
   );
 };
 
-export default PaymentForm;
+export default GoldBuying;
--- src/routes/MarketPlace/GoldBuyingWeekly.jsx
+++ src/routes/MarketPlace/GoldBuyingWeekly.jsx
@@ -2,15 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../hooks/GlobalProvider';
 import { GOLD_LIVE_PRICE } from '../../hooks/APIHooks';
 
+
+import {GoldCoin} from '../../assets/data/Imagedata';
+
 const GoldBuying = () => {
   const { user } = useAuth();
-  const [amount, setAmount] = useState(10);
+  const [amount, setAmount] = useState(0);
   const [gold, setGold] = useState(0);
   const [referralCode, setReferralCode] = useState('');
   const [goldPricePerGram, setGoldPricePerGram] = useState(0);
   const [formattedGold, setFormattedGold] = useState('');
 
   const amountValues = [100, 200, 500, 1000];
+
+  const [formData, setFormData] = useState({
+    merchantTransactionId: '',
+    merchantUserId: 'MUID' + Date.now(),
+    amount: '',
+    merchantOrderId: '',
+    mobileNumber: user.phonenumber,
+    message: 'Order For Digital Gold - Regular',
+    email: user.emailaddress,
+    shortName: 'BAT_DigitalGold_Regular',
+    orderlist: '', // Initialize as empty string
+    referralCode: referralCode,
+  });
 
   useEffect(() => {
     const fetchGoldPrice = async () => {
@@ -33,7 +49,7 @@ const GoldBuying = () => {
 
   useEffect(() => {
     const calculateGoldAfterGST = () => {
-      const goldAfterGST = gold * (1 - 0.03);
+      const goldAfterGST = gold * (1 - 0.03); // Assuming 3% GST
       const formattedGold = goldAfterGST.toFixed(8);
       setFormattedGold(formattedGold);
     };
@@ -43,137 +59,118 @@ const GoldBuying = () => {
 
   const handleSliderChange = (e) => {
     const value = Number(e.target.value);
-    setAmount(amountValues[value]);
-    setGold(amountValues[value] / goldPricePerGram);
+    const selectedAmount = amountValues[value];
+    const calculatedGold = selectedAmount / goldPricePerGram;
+    setAmount(selectedAmount);
+    setGold(calculatedGold);
+
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      amount: selectedAmount,
+      merchantTransactionId: 'MTID' + Date.now(),
+      merchantOrderId: 'MOID' + Date.now(),
+      orderlist: `${calculatedGold.toFixed(8)} grams`, // Update orderlist with calculated gold grams
+    }));
   };
 
   const handleReferralCodeChange = (e) => {
     const code = e.target.value;
     setReferralCode(code);
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      referralCode: code,
+      orderlist: `${formattedGold} grams`, // Update orderlist with referral code
+    }));
   };
 
-  const handlePayment = () => {
-    const options = {
-      key: 'rzp_test_qjbYOaA0BlqnRS',
-      amount: amount * 100,
-      currency: 'INR',
-      name: 'Gold Buying App',
-      description: 'Gold purchase',
-      handler: function(response) {
-        alert('Payment successful. Payment ID: ' + response.razorpay_payment_id);
+  const handlePayment = async (e) => {
+    e.preventDefault();
 
-        const currentDateTime = new Date().toISOString();
-
-        if (amount >= 10) {
-          fetch('https://batchugold.com/(apis)/Store/Orders.php', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              payment_id: response.razorpay_payment_id,
-              amount: amount,
-              email: user.emailaddress,
-              phone: user.phonenumber,
-              referral_code: referralCode,
-              created_at: currentDateTime,
-              notes: {
-                address: 'Gold Buying App Corporate Office',
-                referral_code_gold: referralCode,
-                product_type: 'Regular Savings - Weekly',
-                products: 'Raw Gold',
-              },
-            }),
-          })
-            .then(response => response.json())
-            .then(data => {
-              if (data.success) {
-                alert('Payment processed successfully');
-              } else {
-                alert('Failed to process payment');
-              }
-            })
-            .catch(error => {
-              console.error('Error:', error);
-              alert('Failed to process payment');
-            });
-        }
-      },
-      prefill: {
-        name: user.name || '',
-        email: user.emailaddress,
-        contact: user.phonenumber,
-      },
-      notes: {
-        address: 'Gold Buying App Corporate Office',
-        referral_code_gold: referralCode,
-        product_type: 'Regular Savings',
-        products: 'Raw Gold',
-      },
-      theme: {
-        color: '#3399cc',
-      },
-      modal: {
-        ondismiss: function() {
-          alert('Payment process cancelled.');
+    try {
+      const response = await fetch('https://batchugold.com/apis/PhonePe/DigitalGold/PhonePe.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
         },
-      },
-    };
+        body: JSON.stringify(formData)
+      });
 
-    const rzp = new window.Razorpay(options);
-    rzp.open();
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const responseData = await response.json();
+      if (responseData.error) {
+        console.error('Error:', responseData.error);
+        // Handle error state or show error to user
+      } else {
+        // Redirect to payment URL received from backend
+        window.location.href = responseData.iframeUrl; // Assuming iframeUrl is returned on success
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle error state or show error to user
+    }
   };
 
   return (
-    <div className="py-10 flex flex-col items-center justify-center bg-gray-100 min-h-screen">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4">Weekly Savings</h1>
-        <label className="block mb-2 text-gray-700">Select Amount (INR):</label>
-        <input
-          type="range"
-          min="0"
-          max={amountValues.length - 1}
-          step="1"
-          value={amountValues.indexOf(amount)}
-          onChange={handleSliderChange}
-          className="w-full"
-        />
-        <div className="text-center mt-2">
-          Rs. {amount} will be saved daily
+    <div className="p-5 flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+      
+      <div className="p-8 w-full max-w-md">
+
+
+      <div className="bg-gradient-to-r from-[#facc15] to-[#ca8a04] p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-4 text-center text-white">Buy Gold Weekly</h1>
+        <label className="block mb-2 text-white font-bold ">Select Amount (INR):</label>
+
+        <div className="relative w-full bg-yellow-700 max-w-lg h-10 flex items-center rounded-lg">
+          <input
+            type="range"
+            min="0"
+            max={amountValues.length - 1}
+            step="1"
+            value={amountValues.indexOf(amount)}
+            onChange={handleSliderChange}
+            className="w-full appearance-none bg-transparent cursor-pointer "
+            style={{ zIndex: 10, position: 'relative' }}
+          />
+          <div
+            className="absolute top-1/2 transform -translate-y-1/2 h-8 w-8 flex items-center justify-center pointer-events-none"
+            style={{
+              left: `calc(${(amountValues.indexOf(amount) / (amountValues.length - 1)) * 100}% - 16px)`,
+              zIndex: 20
+            }}
+          >
+            <img src={GoldCoin} alt="indicator" className="h-full w-full" />
+          </div>
         </div>
-        {user && (
-          <>
-            <label className="block mb-2 text-gray-700">Email:</label>
-            <input
-              type="email"
-              value={user.emailaddress}
-              readOnly
-              className="w-full px-3 py-2 mb-4 border rounded bg-gray-200"
-            />
-            <label className="block mb-2 text-gray-700">Phone Number:</label>
-            <input
-              type="text"
-              value={user.phonenumber}
-              readOnly
-              className="w-full px-3 py-2 mb-4 border rounded bg-gray-200"
-            />
-          </>
-        )}
-        <label className="block mb-2 text-gray-700">Referral Code:</label>
-        <input
-          type="text"
-          value={referralCode}
-          onChange={handleReferralCodeChange}
-          className="w-full px-3 py-2 mb-4 border rounded"
-        />
-        <p className="mb-4">Gold you will receive after GST: {formattedGold} grams</p>
+
+        <div className="text-center mt-1">
+          Rs. {amount}
+        </div>
+       
+        <p className="mb-4 text-[12px]"> 
+          Receive: {formattedGold} grams
+        </p>
         <button
           onClick={handlePayment}
-          className="w-full bg-teal-900 text-white py-2 rounded hover:bg-teal-600"
+          className="w-full bg-yellow-900 text-white py-2 rounded hover:bg-yellow-700"
         >
           Buy Now
         </button>
       </div>
+
+     <div className='px-5'>
+     <div className='p-5 bg-yellow-400 rounded-b-2xl'>
+      <label className="block mb-2 text-yellow-900 font-bold text-sm ">Referral Code:</label>
+        <input
+          type="text"
+          value={referralCode}
+          onChange={handleReferralCodeChange}
+          className="w-full px-2 mb-2 border rounded "
+        /></div></div>
+
+    </div>
     </div>
   );
 };
